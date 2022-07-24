0acd55d9a6ff660b2cc186add752575c97efe086
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-07-24 15:13:57 +0530

    readable content guide

---
+++ ReadableContentGuide_5/ReadableContentGuide_5/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ReadableContentGuide_5
+//
+//  Created by Mahesh Kulkarni on 24/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setUpView()
+    }
+
+
+    func setUpView() {
+        let label = makeLabel(withText: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in.")
+        self.view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor),
+            label.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+            label.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor)
+        ])
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.numberOfLines = 0
+        label.backgroundColor = .yellow
+        return label
+    }
+    
+}
+
